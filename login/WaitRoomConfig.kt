a6a939395784ef028e31a9145f8b7e4ae674ba63
OctavioPeace <75342539+OctavioPeace@users.noreply.github.com>
2021-11-22 21:56:04 -0600

    Add files via upload

--- login/WaitRoomConfig.kt
+++ login/WaitRoomConfig.kt
@@ -1,19 +1,56 @@
 package com.example.login
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
 import com.example.login.databinding.WaitroomLayoutBinding
+import kotlin.math.roundToInt
 
 class WaitRoomConfig : AppCompatActivity() {
     private lateinit var binding: WaitroomLayoutBinding
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = WaitroomLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnExit.setOnClickListener { finish() }
+        binding.btnExit.setOnClickListener {
+            stopService(serviceIntent)
+            finish()
+        }
 
+        serviceIntent = Intent(applicationContext, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
+
+        startTimer()
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(TimerService.TIMER_EXTRA, 0.0)
+            binding.txtCrono.text = getStringFromDouble(time)
+        }
+    }
+
+    private fun getStringFromDouble(time: Double): String {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String = String.format("%02d:%02d:%02d", hour, min, sec)
+
+    private fun startTimer() {
+        serviceIntent.putExtra(TimerService.TIMER_EXTRA, time)
+        startService(serviceIntent)
     }
 }
