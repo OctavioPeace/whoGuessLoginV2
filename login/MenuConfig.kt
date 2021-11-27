c3776886a468c9152f87817e843452eb0ad8afde
OctavioPeace <75342539+OctavioPeace@users.noreply.github.com>
2021-11-26 19:08:53 -0600

    Add files via upload

--- login/MenuConfig.kt
+++ login/MenuConfig.kt
@@ -2,10 +2,14 @@ package com.example.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuInflater
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.login.databinding.MenuLayoutBinding
 
-class MenuConfig : AppCompatActivity(){
+class MenuConfig : AppCompatActivity() {
     private lateinit var binding: MenuLayoutBinding
 
     public override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,4 +29,34 @@ class MenuConfig : AppCompatActivity(){
             startActivity(intent)
         }
     }
+
+    fun showPopup(v : View){
+        val popup = PopupMenu(this, v)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.popup_menu, popup.menu)
+        popup.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.item1-> {
+                    Toast.makeText(this, "Item1", Toast.LENGTH_SHORT).show()
+                    true
+                }
+            }
+            true
+        }
+        popup.show()
+    }
+
+    fun showMenu(v: View) {
+        PopupMenu(this, v).apply {
+            // MainActivity implements OnMenuItemClickListener
+            setOnMenuItemClickListener(this@MenuConfig)
+            inflate(R.menu.popup_menu)
+            show()
+        }
+    }
+
+    private fun setOnMenuItemClickListener(menuConfig: MenuConfig) {
+
+    }
 }
+
